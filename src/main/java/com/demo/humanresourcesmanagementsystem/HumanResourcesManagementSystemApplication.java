c775df0a48bb2969577f16367c5842b0e3604a0a
Hasan Can Ozbek <hasancanozbekk@gmail.com>
2022-01-23 22:45:57 +0300

    Swagger support added.

--- src/main/java/com/demo/humanresourcesmanagementsystem/HumanResourcesManagementSystemApplication.java
+++ src/main/java/com/demo/humanresourcesmanagementsystem/HumanResourcesManagementSystemApplication.java
@@ -1,9 +1,12 @@
 package com.demo.humanresourcesmanagementsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "HRMS API", version = "1.0", description = "Human Resources Management System"))
 public class HumanResourcesManagementSystemApplication {
 
     public static void main(String[] args) {
